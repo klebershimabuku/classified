03619895d2b6a82986350bd4749242a9857d8afb
Kleber Shimabuku <klebershimabuku@gmail.com>
2012-03-10 19:51:15 +0900

    remove old file

--- lib/validators/limit_validator.rb
+++
@@ -1,9 +0,0 @@
-class LimitValidator < ActiveModel::EachValidator
-
-  MAX_ATTACHMENTS = 3
-
-  def validate_each(record, attribute, value)
-    record.errors.add(attribute,"#{Constants::MAX_ATTACHMENTS} attachments per post only.") if attribute.size > Constants::MAX_ATTACHMENTS
-  end  
-  
-end
